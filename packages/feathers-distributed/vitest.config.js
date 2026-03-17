f5f45be87b07c1361d137c20dde3d47b81dbbf84
Robin Bourianes <robin.bourianes@kalisio.com>
2026-03-17 11:52:10 +0100

    wip: make use of base vite and vitest configs

--- packages/feathers-distributed/vitest.config.js
+++ packages/feathers-distributed/vitest.config.js
@@ -1,34 +1,19 @@
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { baseConfig } from '../../vitest.base-config'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export default defineConfig({
+export default mergeConfig(baseConfig, defineConfig({
   root: __dirname,
   test: {
     name: 'feathers-distributed',
     environment: 'node',
-    globals: true,
-    silent: false,
     // beforeAll hook needs a lot of time to setup cote and stuff
     hookTimeout: 30000,
-    testTimeout: 30000,
+    // testTimeout: 30000,
     // Disable parallelism because different test files startup listening servers on same ports
-    fileParallelism: false,
-    coverage: {
-      provider: 'v8',
-      all: true,
-      clean: true,
-      include: ['src/**/*.js'],
-      exclude: [
-        'node_modules/**',
-        'dist/**',
-        'test/**',
-        '*.config.js'
-      ],
-      reporter: ['text', 'html', 'lcov'],
-      reportsDirectory: './coverage'
-    }
+    fileParallelism: false
   }
-})
+}))
