e154f0f1ce8735d04453ed865c46635ff0152de4
Robin Bourianes <robin.bourianes@kalisio.com>
2026-03-17 21:09:38 +0100

    chore(feathers-distributed): vitest config cleanup

--- packages/feathers-distributed/vitest.config.js
+++ packages/feathers-distributed/vitest.config.js
@@ -12,10 +12,5 @@ export default mergeConfig(baseConfig, defineConfig({
     environment: 'node',
     // beforeAll hook needs a lot of time to setup cote and stuff
     hookTimeout: 30000
-    // testTimeout: 30000,
-    // Disable parallelism because tests fail otherwise, not sure exactly why.
-    // Different files don't share same server ports, but touching the basePort in cote config seems
-    // to break tests in index.test.js ... So currently files share the same cote basePort value.
-    // fileParallelism: false
   }
 }))
