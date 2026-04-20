d2ff7ee7a756ea40a5afaae509a9c31e9be03b2d
cnouguier <ch.nouguier@gmail.com>
2026-04-20 19:12:33 +0200

    wip: deleteExpiredSubscriptions hook not removing expired subscriptions #14

--- packages/feathers-webpush/src/server/hooks.js
+++ packages/feathers-webpush/src/server/hooks.js
@@ -9,20 +9,20 @@ export async function deleteExpiredSubscriptions (hook) {
   }
   const app = hook.app
   const subscriptionService = hook.result.subscriptionService
-  const resultFailed = hook.result.failed
+  const errors = _.filter(hook.result.failed, error => (error.statusCode === 410 || error.statusCode === 404))
   const subscriptionProperty = hook.result.subscriptionProperty
   const subscriptions = await app.service(subscriptionService).find({ paginate: false })
-  _.forEach(_.filter(resultFailed, error => (error.statusCode === 410 || error.statusCode === 404)), async error => {
-    _.forEach(subscriptions, async subscription => {
-      if (_.has(subscription, subscriptionProperty) && _.find(_.get(subscription, subscriptionProperty), subscription => subscription.endpoint === error.endpoint)) {
+  for (const error of errors) {
+    for (const subscription of subscriptions) {
+      if (_.find(_.get(subscription, subscriptionProperty), sub => sub.endpoint === error.endpoint)) {
         // Patch subscriptions
         _.set(subscription, subscriptionProperty, _.filter(_.get(subscription, subscriptionProperty, []), subscription => subscription.endpoint !== error.endpoint))
         const newSubscriptionProperty = {}
         newSubscriptionProperty[subscriptionProperty] = _.get(subscription, subscriptionProperty)
-        app.service(subscriptionService).patch(subscription._id, newSubscriptionProperty)
+        await app.service(subscriptionService).patch(subscription._id, newSubscriptionProperty)
         debug(`Delete subscription with endpoint ${error.endpoint}`)
       }
-    })
-  })
+    }
+  }
   return hook
 }
