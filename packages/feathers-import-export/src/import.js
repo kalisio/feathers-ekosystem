0c9024d6bf1766cc9a1063b5082b6dd101fcbbf5
cnouguier <ch.nouguier@gmail.com>
2026-09-01 15:06:20 +0200

    chore(feathers-import-export): fix operator transform imports

--- packages/feathers-import-export/src/import.js
+++ packages/feathers-import-export/src/import.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import { promisify } from 'node:util'
 import { pipeline, Writable } from 'node:stream'
-import { transform } from '@kalisio/common-core/transform'
+import { transform } from '@kalisio/common-core/operators/transform'
 import { importers } from './importers/index.js'
 
 import createDebug from 'debug'
